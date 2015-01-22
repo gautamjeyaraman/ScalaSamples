f1dd87ce543faca0dbba33cea549968312b12c3f
gautamjeyaraman <gautam.jeyaraman@tataatsu.com>
2015-01-22 18:39:54 +0530

    Hello World and Variables

---
+++ 02_Variables/Variables.scala
@@ -0,0 +1,10 @@
+object Variables{
+    def main(args: Array[String]){
+        var aVar:String = "Check one"
+        val bVar = 2
+        println(aVar)
+        println(bVar)
+        aVar = "Check three"
+        println(aVar)
+    }
+}
